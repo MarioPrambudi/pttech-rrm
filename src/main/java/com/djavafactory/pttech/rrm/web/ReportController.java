4eeba9e7e30fe6feef8074e0f41a9e99a3a1a7d9
Carine Leong <carinelpy@hotmail.com>
2011-04-28 17:39:37 +0800

    Add reports into configuration

--- src/main/java/com/djavafactory/pttech/rrm/web/ReportController.java
+++ src/main/java/com/djavafactory/pttech/rrm/web/ReportController.java
@@ -1,6 +1,5 @@
 package com.djavafactory.pttech.rrm.web;
 
-import com.djavafactory.pttech.rrm.Constants;
 import com.djavafactory.pttech.rrm.domain.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,19 +13,23 @@ import java.util.List;
 @RequestMapping("/reports")
 @Controller
 public class ReportController {
+
+    public static final String REPORT_TYPE_CELCOM = "celcom";
+    public static final String REPORT_TYPE_TNG = "tng";
+
     @RequestMapping(method = RequestMethod.GET)
     public String getReportList(@RequestParam(value = "type") String type, Model uiModel) {
         List<Configuration> configList = null;
-        if(StringUtils.endsWithIgnoreCase(type, Constants.REPORT_TYPE_CELCOM)) {
-            configList = Configuration.findConfigurationsByConfigKeyLike("report.celcom").getResultList(); //todo Use Enum or ?
-        } else if(StringUtils.endsWithIgnoreCase(type, Constants.REPORT_TYPE_TNG)) {
-            configList = Configuration.findConfigurationsByConfigKeyLike("report.tng").getResultList(); //todo Use Enum or ?
+        if (StringUtils.endsWithIgnoreCase(type, REPORT_TYPE_CELCOM)) {
+            configList = Configuration.findConfigurationsByConfigKeyLike(Configuration.ConfigPrefix.REPORT_CELCOM.getKey()).getResultList();
+        } else if (StringUtils.endsWithIgnoreCase(type, REPORT_TYPE_TNG)) {
+            configList = Configuration.findConfigurationsByConfigKeyLike(Configuration.ConfigPrefix.REPORT_TNG.getKey()).getResultList();
         }
-        if(configList != null && !configList.isEmpty()) {
-            for(int i = 0; i < configList.size(); i++) {
+        if (configList != null && !configList.isEmpty()) {
+            for (int i = 0; i < configList.size(); i++) {
                 Configuration config = configList.get(i);
-                if(config.getConfigKey().lastIndexOf(".") > -1) {
-                    config.setConfigKey(config.getConfigKey().substring(config.getConfigKey().lastIndexOf(".")+1));
+                if (config.getConfigKey().lastIndexOf(".") > -1) {
+                    config.setConfigKey(config.getConfigKey().substring(config.getConfigKey().lastIndexOf(".") + 1));
                 }
             }
         }
