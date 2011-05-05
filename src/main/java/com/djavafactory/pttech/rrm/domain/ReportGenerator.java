2e801fe627527f86964662669b8464e2ea8209ea
rain poh <rainpoh@djavafactory.com>
2011-05-05 10:03:03 +0800

    get latest code

---
+++ src/main/java/com/djavafactory/pttech/rrm/domain/ReportGenerator.java
@@ -0,0 +1,11 @@
+package com.djavafactory.pttech.rrm.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rainpoh
+ * Date: 5/5/11
+ * Time: 9:25 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReportGenerator {
+}
